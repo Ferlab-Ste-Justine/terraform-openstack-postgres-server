01566b082eabef2ad4a3f5922d55007d3d014359
Eric Vallee <eric_vallee@webificservices.com>
2023-10-02 00:53:07 -0400

    feat: INFRA-240 Initial commit

---
+++ main.tf
@@ -0,0 +1,231 @@
+locals {
+  fluentbit_updater_etcd = var.fluentbit.enabled && var.fluentbit_dynamic_config.enabled && var.fluentbit_dynamic_config.source == "etcd"
+  fluentbit_updater_git = var.fluentbit.enabled && var.fluentbit_dynamic_config.enabled && var.fluentbit_dynamic_config.source == "git"
+  block_devices = var.image_source.volume_id != "" ? [{
+    uuid                  = var.image_source.volume_id
+    source_type           = "volume"
+    boot_index            = 0
+    destination_type      = "volume"
+    delete_on_termination = false
+  }] : []
+}
+
+module "postgres_configs" {
+  source = "git::https://github.com/Ferlab-Ste-Justine/terraform-cloudinit-templates.git//postgres?ref=v0.14.0"
+  install_dependencies = var.install_dependencies
+  advertise_ip = var.network_port.all_fixed_ips.0
+  etcd = var.etcd
+  postgres = {
+    replicator_password = var.postgres.replicator_password
+    superuser_password  = var.postgres.superuser_password
+    ca_cert             = var.postgres.ca_certificate
+    server_cert         = "${var.postgres.server_certificate}\n${var.postgres.ca_certificate}"
+    server_key          = var.postgres.server_key
+    params              = var.postgres.params
+  }
+  patroni = {
+    scope                  = var.patroni.scope
+    namespace              = var.patroni.namespace
+    name                   = var.patroni.name
+    ttl                    = var.patroni.ttl
+    loop_wait              = var.patroni.loop_wait
+    retry_timeout          = var.patroni.retry_timeout
+    master_start_timeout   = var.patroni.master_start_timeout
+    master_stop_timeout    = var.patroni.master_stop_timeout
+    watchdog_safety_margin = var.patroni.watchdog_safety_margin
+    synchronous_node_count = var.patroni.synchronous_node_count
+    api                    = {
+      ca_cert       = var.postgres.ca_certificate
+      server_cert   = "${var.postgres.server_certificate}\n${var.postgres.ca_certificate}"
+      server_key    = var.postgres.server_key
+      client_cert   = var.patroni.client_certificate
+      client_key    = var.patroni.client_key
+    }
+  }    
+}
+
+module "prometheus_node_exporter_configs" {
+  source = "git::https://github.com/Ferlab-Ste-Justine/terraform-cloudinit-templates.git//prometheus-node-exporter?ref=v0.14.0"
+  install_dependencies = var.install_dependencies
+}
+
+module "chrony_configs" {
+  source = "git::https://github.com/Ferlab-Ste-Justine/terraform-cloudinit-templates.git//chrony?ref=v0.14.0"
+  install_dependencies = var.install_dependencies
+  chrony = {
+    servers  = var.chrony.servers
+    pools    = var.chrony.pools
+    makestep = var.chrony.makestep
+  }
+}
+
+
+module "fluentbit_updater_etcd_configs" {
+  source = "git::https://github.com/Ferlab-Ste-Justine/terraform-cloudinit-templates.git//configurations-auto-updater?ref=v0.14.0"
+  install_dependencies = var.install_dependencies
+  filesystem = {
+    path = "/etc/fluent-bit-customization/dynamic-config"
+    files_permission = "700"
+    directories_permission = "700"
+  }
+  etcd = {
+    key_prefix = var.fluentbit_dynamic_config.etcd.key_prefix
+    endpoints = var.fluentbit_dynamic_config.etcd.endpoints
+    connection_timeout = "60s"
+    request_timeout = "60s"
+    retry_interval = "4s"
+    retries = 15
+    auth = {
+      ca_certificate = var.fluentbit_dynamic_config.etcd.ca_certificate
+      client_certificate = var.fluentbit_dynamic_config.etcd.client.certificate
+      client_key = var.fluentbit_dynamic_config.etcd.client.key
+      username = var.fluentbit_dynamic_config.etcd.client.username
+      password = var.fluentbit_dynamic_config.etcd.client.password
+    }
+  }
+  notification_command = {
+    command = ["/usr/local/bin/reload-fluent-bit-configs"]
+    retries = 30
+  }
+  naming = {
+    binary = "fluent-bit-config-updater"
+    service = "fluent-bit-config-updater"
+  }
+  user = "fluentbit"
+}
+
+module "fluentbit_updater_git_configs" {
+  source = "git::https://github.com/Ferlab-Ste-Justine/terraform-cloudinit-templates.git//gitsync?ref=v0.14.0"
+  install_dependencies = var.install_dependencies
+  filesystem = {
+    path = "/etc/fluent-bit-customization/dynamic-config"
+    files_permission = "700"
+    directories_permission = "700"
+  }
+  git = var.fluentbit_dynamic_config.git
+  notification_command = {
+    command = ["/usr/local/bin/reload-fluent-bit-configs"]
+    retries = 30
+  }
+  naming = {
+    binary = "fluent-bit-config-updater"
+    service = "fluent-bit-config-updater"
+  }
+  user = "fluentbit"
+}
+
+module "fluentbit_configs" {
+  source = "git::https://github.com/Ferlab-Ste-Justine/terraform-cloudinit-templates.git//fluent-bit?ref=v0.14.0"
+  install_dependencies = var.install_dependencies
+  fluentbit = {
+    metrics = var.fluentbit.metrics
+    systemd_services = [
+      {
+        tag     = var.fluentbit.patroni_tag
+        service = "patroni.service"
+      },
+      {
+        tag = var.fluentbit.node_exporter_tag
+        service = "node-exporter.service"
+      }
+    ]
+    forward = var.fluentbit.forward
+  }
+  dynamic_config = {
+    enabled = var.fluentbit_dynamic_config.enabled
+    entrypoint_path = "/etc/fluent-bit-customization/dynamic-config/index.conf"
+  }
+}
+
+locals {
+  cloudinit_templates = concat([
+      {
+        filename     = "base.cfg"
+        content_type = "text/cloud-config"
+        content = templatefile(
+          "${path.module}/files/user_data.yaml.tpl", 
+          {
+            hostname = var.name
+            install_dependencies = var.install_dependencies
+          }
+        )
+      },
+      {
+        filename     = "node_exporter.cfg"
+        content_type = "text/cloud-config"
+        content      = module.prometheus_node_exporter_configs.configuration
+      },
+      {
+        filename     = "postgres.cfg"
+        content_type = "text/cloud-config"
+        content      = module.postgres_configs.configuration
+      },
+    ],
+    var.chrony.enabled ? [{
+      filename     = "chrony.cfg"
+      content_type = "text/cloud-config"
+      content      = module.chrony_configs.configuration
+    }] : [],
+    local.fluentbit_updater_etcd ? [{
+      filename     = "fluent_bit_updater.cfg"
+      content_type = "text/cloud-config"
+      content      = module.fluentbit_updater_etcd_configs.configuration
+    }] : [],
+    local.fluentbit_updater_git ? [{
+      filename     = "fluent_bit_updater.cfg"
+      content_type = "text/cloud-config"
+      content      = module.fluentbit_updater_git_configs.configuration
+    }] : [],
+    var.fluentbit.enabled ? [{
+      filename     = "fluent_bit.cfg"
+      content_type = "text/cloud-config"
+      content      = module.fluentbit_configs.configuration
+    }] : []
+  )
+}
+
+data "cloudinit_config" "user_data" {
+  gzip = true
+  base64_encode = true
+  dynamic "part" {
+    for_each = local.cloudinit_templates
+    content {
+      filename     = part.value["filename"]
+      content_type = part.value["content_type"]
+      content      = part.value["content"]
+    }
+  }
+}
+
+resource "openstack_compute_instance_v2" "postgres_server" {
+  name            = var.name
+  image_id        = var.image_source.image_id != "" ? var.image_source.image_id : null
+  flavor_id       = var.flavor_id
+  key_pair        = var.keypair_name
+  user_data = data.cloudinit_config.user_data.rendered
+
+  network {
+    port = var.network_port.id
+  }
+
+  dynamic "block_device" {
+    for_each = local.block_devices
+    content {
+      uuid                  = block_device.value["uuid"]
+      source_type           = block_device.value["source_type"]
+      boot_index            = block_device.value["boot_index"]
+      destination_type      = block_device.value["destination_type"]
+      delete_on_termination = block_device.value["delete_on_termination"]
+    }
+  }
+
+  scheduler_hints {
+    group = var.server_group.id
+  }
+
+  lifecycle {
+    ignore_changes = [
+      user_data,
+    ]
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,210 @@
+variable "name" {
+  description = "Name to give to the vm."
+  type        = string
+}
+
+variable "network_port" {
+  description = "Network port to assign to the node. Should be of type openstack_networking_port_v2"
+  type        = any
+}
+
+variable "server_group" {
+  description = "Server group to assign to the node. Should be of type openstack_compute_servergroup_v2"
+  type        = any
+}
+
+variable "image_source" {
+  description = "Source of the vm's image"
+  type = object({
+    image_id = string
+    volume_id = string
+  })
+}
+
+variable "flavor_id" {
+  description = "ID of the VM flavor"
+  type = string
+}
+
+variable "keypair_name" {
+  description = "Name of the keypair that will be used by admins to ssh to the node"
+  type = string
+}
+
+variable "chrony" {
+  description = "Chrony configuration for ntp. If enabled, chrony is installed and configured, else the default image ntp settings are kept"
+  type        = object({
+    enabled = bool,
+    //https://chrony.tuxfamily.org/doc/4.2/chrony.conf.html#server
+    servers = list(object({
+      url = string,
+      options = list(string)
+    })),
+    //https://chrony.tuxfamily.org/doc/4.2/chrony.conf.html#pool
+    pools = list(object({
+      url = string,
+      options = list(string)
+    })),
+    //https://chrony.tuxfamily.org/doc/4.2/chrony.conf.html#makestep
+    makestep = object({
+      threshold = number,
+      limit = number
+    })
+  })
+  default = {
+    enabled = false
+    servers = []
+    pools = []
+    makestep = {
+      threshold = 0,
+      limit = 0
+    }
+  }
+}
+
+variable "fluentbit" {
+  description = "Fluent-bit configuration"
+  type = object({
+    enabled = bool
+    patroni_tag = string
+    node_exporter_tag = string
+    metrics = object({
+      enabled = bool
+      port    = number
+    })
+    forward = object({
+      domain = string
+      port = number
+      hostname = string
+      shared_key = string
+      ca_cert = string
+    })
+  })
+  default = {
+    enabled = false
+    patroni_tag = ""
+    node_exporter_tag = ""
+    metrics = {
+      enabled = false
+      port = 0
+    }
+    forward = {
+      domain = ""
+      port = 0
+      hostname = ""
+      shared_key = ""
+      ca_cert = ""
+    }
+  }
+}
+
+variable "fluentbit_dynamic_config" {
+  description = "Parameters for fluent-bit dynamic config if it is enabled"
+  type = object({
+    enabled = bool
+    source  = string
+    etcd    = object({
+      key_prefix     = string
+      endpoints      = list(string)
+      ca_certificate = string
+      client         = object({
+        certificate = string
+        key         = string
+        username    = string
+        password    = string
+      })
+    })
+    git     = object({
+      repo             = string
+      ref              = string
+      path             = string
+      trusted_gpg_keys = list(string)
+      auth             = object({
+        client_ssh_key         = string
+        server_ssh_fingerprint = string
+      })
+    })
+  })
+  default = {
+    enabled = false
+    source = "etcd"
+    etcd = {
+      key_prefix     = ""
+      endpoints      = []
+      ca_certificate = ""
+      client         = {
+        certificate = ""
+        key         = ""
+        username    = ""
+        password    = ""
+      }
+    }
+    git  = {
+      repo             = ""
+      ref              = ""
+      path             = ""
+      trusted_gpg_keys = []
+      auth             = {
+        client_ssh_key         = ""
+        server_ssh_fingerprint = ""
+      }
+    }
+  }
+
+  validation {
+    condition     = contains(["etcd", "git"], var.fluentbit_dynamic_config.source)
+    error_message = "fluentbit_dynamic_config.source must be 'etcd' or 'git'."
+  }
+}
+
+variable "postgres" {
+  description = "Postgres configurations"
+  sensitive   = true
+  type = object({
+    params = list(object({
+      key = string,
+      value = string,
+    })),
+    replicator_password = string,
+    superuser_password = string,
+    ca_certificate = string,
+    server_certificate = string,
+    server_key = string,
+  })
+}
+
+variable "etcd" {
+  description = "Etcd configurations"
+  sensitive   = true
+  type = object({
+      endpoints = list(string),
+      ca_cert = string,
+      username = string,
+      password = string,
+  })
+}
+
+variable "patroni" {
+  description = "Patroni configurations"
+  sensitive   = true
+  type = object({
+    scope = string,
+    namespace = string,
+    name = string,
+    ttl = number,
+    loop_wait = number,
+    retry_timeout = number,
+    master_start_timeout = number,
+    master_stop_timeout = number,
+    watchdog_safety_margin = number,
+    synchronous_node_count = number,
+    client_certificate = string,
+    client_key = string,
+  })
+}
+
+variable "install_dependencies" {
+  description = "Whether to install all dependencies in cloud-init"
+  type = bool
+  default = true
+}
